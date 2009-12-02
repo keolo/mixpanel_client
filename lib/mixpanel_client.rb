71686fed56bc695eecc66262ddac19d32f199cb5
Keolo Keagy <keolo@dreampointmedia.com>
2009-12-02 14:12:40 -0800

    Added some tests and minor cleanup.

--- lib/mixpanel_client.rb
+++ lib/mixpanel_client.rb
@@ -3,7 +3,7 @@
 # Mixpanel API Ruby Client Library
 #
 # Copyright (c) 2009+ Keolo Keagy
-# MIT License http://www.opensource.org/licenses/mit-license.php
+# See LICENSE for details.
 # Open sourced by the good folks at SharesPost.com
 #
 # Inspired by the official mixpanel php and python libraries.
@@ -12,6 +12,7 @@
 require 'cgi'
 require 'digest/md5'
 require 'open-uri'
+require 'rubygems'
 require 'json'
 
 module Mixpanel
@@ -22,11 +23,11 @@ module Mixpanel
     VERSION  = '1.0'
 
     def initialize(config)
-      @api_key = config[:api_key]
+      @api_key    = config[:api_key]
       @api_secret = config[:api_secret]
     end
 
-    def request(endpoint, method, params)
+    def request(endpoint, meth, params)
       params[:api_key]  = api_key
       params[:expire]   = Time.now.to_i + 600 # Grant this request 10 minutes
       params[:format] ||= :json
@@ -34,7 +35,7 @@ module Mixpanel
 
       @format = params[:format]
 
-      response = URI.parse("#{BASE_URI}/#{endpoint}/#{VERSION}/#{method}?#{urlencode(params)}").read
+      response = URI.parse("#{BASE_URI}/#{endpoint}/#{VERSION}/#{meth}?#{urlencode(params)}").read
       to_hash(response)
     end
 
@@ -43,14 +44,15 @@ module Mixpanel
     end
 
     def urlencode(params)
-      params.map{|k,v| "#{k}=#{CGI.escape(v.to_s)}"}.join('&')
+      params.map{|k,v| "#{k}=#{CGI.escape(v.to_s)}"}.sort.join('&')
     end
 
-    def to_hash(data)
-      case @format
-      when :json
-        JSON.parse(data)
+    protected
+      def to_hash(data)
+        case @format
+        when :json
+          JSON.parse(data)
+        end
       end
-    end
   end
 end
--- spec/mixpanel_client_spec.rb
+++ spec/mixpanel_client_spec.rb
@@ -1,7 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "MixpanelClient" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe 'Mixpanel::Client' do
+  before :all do
+    config = {:api_key => 'test', :api_secret => 'test'}
+    @api = Mixpanel::Client.new(config)
+  end
+
+  describe '#hash_args' do
+    it 'should return a hashed string alpha sorted by key names.' do
+      args              = {:c => 'see', :a => 'aye', :d => 'dee', :b => 'bee'}
+      args_alpha_sorted = {:a => 'aye', :b => 'bee', :c => 'see', :d => 'dee'}
+      @api.hash_args(args).should == @api.hash_args(args_alpha_sorted)
+    end
+  end
+
+  describe '#urlencode' do
+    it 'should return a string with url encoded values.' do
+      params = {:hey => '!@#$%^&*()\/"Ü', :soo => "hëllö?"}
+      @api.urlencode(params).should == 'hey=%21%40%23%24%25%5E%26%2A%28%29%5C%2F%22%C3%9C&soo=h%C3%ABll%C3%B6%3F'
+    end
   end
 end

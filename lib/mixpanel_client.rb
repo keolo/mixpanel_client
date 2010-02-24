38bde8ee36d9fc7ba676f4254f745e2077f6514f
Keolo Keagy <keolo@dreampointmedia.com>
2010-02-23 23:33:59 -0800

    Refactored to remove code smells and improve overall code quality.

--- lib/mixpanel_client.rb
+++ lib/mixpanel_client.rb
@@ -12,57 +12,57 @@ require 'cgi'
 require 'digest/md5'
 require 'open-uri'
 
+# Ruby library for the mixpanel.com web service
 module Mixpanel
-  class Client
-    attr_accessor :api_key, :api_secret, :format
+  BASE_URI = 'http://mixpanel.com/api'
+  VERSION  = '1.0'
 
-    BASE_URI = 'http://mixpanel.com/api'
-    VERSION  = '1.0'
+  # The mixpanel client can be used to easily consume data through the
+  # mixpanel API.
+  class Client
+    attr_accessor :api_key, :api_secret
 
     def initialize(config)
       @api_key    = config[:api_key]
       @api_secret = config[:api_secret]
-      @format   ||= :json
     end
 
     def request(endpoint, meth, params)
-      params[:api_key]  = api_key
-      params[:expire]   = Time.now.to_i + 600 # Grant this request 10 minutes
-      params[:format] ||= @format
-      params[:sig]      = hash_args(params)
-
-      @sig    = params[:sig]
-      @format = params[:format]
-
-      response = get(mixpanel_uri(endpoint, meth, params))
+      uri = URI.mixpanel(endpoint, meth, normalize_params(params))
+      response = URI.get(uri)
       to_hash(response)
     end
 
+    def normalize_params(params)
+      params.merge!(
+        :api_key => api_key,
+        :expire  => Time.now.to_i + 600, # Grant this request 10 minutes
+        :format  => :json
+      ).merge!(:sig => hash_args(params))
+    end
+
     def hash_args(args)
-      Digest::MD5.hexdigest(args.map{|k,v| "#{k}=#{v}"}.sort.join + api_secret)
-    end
-
-    def get(uri)
-      URI.parse(uri).read
-    end
-
-    def mixpanel_uri(endpoint, meth, params)
-      %Q(#{BASE_URI}/#{endpoint}/#{VERSION}/#{meth}?#{urlencode(params)})
-    end
-
-    def urlencode(params)
-      params.map{|k,v| "#{k}=#{CGI.escape(v.to_s)}"}.sort.join('&')
+      Digest::MD5.hexdigest(args.map{|key,val| "#{key}=#{val}"}.sort.join + api_secret)
     end
 
     def to_hash(data)
-      case @format
-      when :placeholder
-        # This is just a placeholder in case Mixpanel supports alternate
-        # formats. Mixpanel at this time (v1.0) only supports JSON.
-      else # The default response is JSON
-        require 'json' unless defined?(JSON)
-        JSON.parse(data)
-      end
+      require 'json' unless defined?(JSON)
+      JSON.parse(data)
+    end
+  end
+
+  # URI related helpers
+  class URI
+    def self.mixpanel(endpoint, meth, params)
+      %Q(#{BASE_URI}/#{endpoint}/#{VERSION}/#{meth}?#{self.encode(params)})
+    end
+
+    def self.encode(params)
+      params.map{|key,val| "#{key}=#{CGI.escape(val.to_s)}"}.sort.join('&')
+    end
+
+    def self.get(uri)
+      ::URI.parse(uri).read
     end
   end
 end
--- spec/mixpanel_client_spec.rb
+++ spec/mixpanel_client_spec.rb
@@ -2,20 +2,16 @@ require 'rubygems'
 
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe 'Mixpanel::Client' do
+describe Mixpanel::Client do
   before :all do
     config = {:api_key => 'test', :api_secret => 'test'}
     @api = Mixpanel::Client.new(config)
   end
 
-  it 'should have a format attribute that defaults to :json' do
-    @api.format.should == :json
-  end
-
   describe '#request' do
     it 'should return json and convert to a ruby hash' do
       # Stub Mixpanel web service
-      @api.stub!(:get).and_return('{"some" : "thing"}')
+      Mixpanel::URI.stub!(:get).and_return('{"some" : "thing"}')
 
       data = @api.request(:events, :general, {
         :event    => '["test-event"]',
@@ -34,23 +30,25 @@ describe 'Mixpanel::Client' do
     end
   end
 
-  describe '#mixpanel_uri' do
-    it 'should return a properly formatted mixpanel uri as a string' do
-      endpoint, meth, params  = [:events, :general, {:c => 'see', :a => 'aye'}]
-      @api.mixpanel_uri(endpoint, meth, params).should == 'http://mixpanel.com/api/events/1.0/general?a=aye&c=see'
-    end
-  end
-
-  describe '#urlencode' do
-    it 'should return a string with url encoded values.' do
-      params = {:hey => '!@#$%^&*()\/"Ü', :soo => "hëllö?"}
-      @api.urlencode(params).should == 'hey=%21%40%23%24%25%5E%26%2A%28%29%5C%2F%22%C3%9C&soo=h%C3%ABll%C3%B6%3F'
-    end
-  end
-
   describe '#to_hash' do
     it 'should return a ruby hash given json as a string' do
       @api.to_hash('{"a" : "aye", "b" : "bee"}').should == {'a' => 'aye', 'b' => 'bee'}
+    end
+  end
+end
+
+describe Mixpanel::URI do
+  describe '.mixpanel' do
+    it 'should return a properly formatted mixpanel uri as a string' do
+      endpoint, meth, params  = [:events, :general, {:c => 'see', :a => 'aye'}]
+      Mixpanel::URI.mixpanel(endpoint, meth, params).should == 'http://mixpanel.com/api/events/1.0/general?a=aye&c=see'
+    end
+  end
+
+  describe '.encode' do
+    it 'should return a string with url encoded values.' do
+      params = {:hey => '!@#$%^&*()\/"Ü', :soo => "hëllö?"}
+      Mixpanel::URI.encode(params).should == 'hey=%21%40%23%24%25%5E%26%2A%28%29%5C%2F%22%C3%9C&soo=h%C3%ABll%C3%B6%3F'
     end
   end
 end

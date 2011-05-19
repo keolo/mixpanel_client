c31d11ef22765259dfc0fa2ba94047d5d050af3d
Keolo Keagy <keolo@dreampointmedia.com>
2011-05-18 17:52:12 -0700

    Followup to last commit.

---
+++ spec/mixpanel_client/properties_externalspec.rb
@@ -0,0 +1,37 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+WebMock.allow_net_connect!
+
+describe 'External calls to mixpanel' do
+  before :all do
+    config = YAML.load_file(File.dirname(__FILE__) + '/../../config/mixpanel.yml')
+    config.should_not be_nil
+    @client = Mixpanel::Client.new(config)
+  end
+
+  context 'when requesting event properties' do
+    it 'should raise an error for bad requests' do
+      data = lambda {
+        @client.request do
+          resource 'properties'
+        end
+      }
+      data.should raise_error(Mixpanel::URI::HTTPError)
+    end
+
+    it 'should return events' do
+      data = @client.request do
+        resource 'events/properties'
+        event    '["test-event"]'
+        name     'hello'
+        values   '["uno", "dos"]'
+        type     'general'
+        unit     'hour'
+        interval  24
+        limit     5
+        bucket   'kicked'
+      end
+      data.should_not be_a Exception
+    end
+  end
+end

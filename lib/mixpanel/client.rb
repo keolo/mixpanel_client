445dfa01241b410b9063d4efd8f253fc23609abc
Alex Fishman <alex@delectable.com>
2012-05-08 15:21:57 -0700

    added length option for api

--- lib/mixpanel/client.rb
+++ lib/mixpanel/client.rb
@@ -17,7 +17,7 @@ module Mixpanel
 
     # Available options for a Mixpanel API request
     OPTIONS = [:resource, :event, :funnel_id, :name, :type, :unit, :interval, :limit, :format, :bucket,
-               :values, :from_date, :to_date, :on, :where, :buckets, :timezone, :length, :nonsense]
+               :values, :from_date, :to_date, :on, :where, :buckets, :timezone, :length]
 
     # Dynamically define accessor methods for each option
     OPTIONS.each do |option|
